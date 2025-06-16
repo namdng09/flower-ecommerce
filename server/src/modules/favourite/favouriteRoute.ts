2de003f5547332b12bb5f435280160dc58664c5f
lnvh <lenguyenviethung1509@gmail.com>
2025-06-16 21:44:58 +0700

    fix: resolve favourite routes with authenticate

--- server/src/modules/favourite/favouriteRoute.ts
+++ server/src/modules/favourite/favouriteRoute.ts
@@ -4,7 +4,7 @@ import { favouriteController } from './favouriteController';
 const router: Router = express.Router();
 router.get('/', asyncHandler(favouriteController.list));
 router.post('/', asyncHandler(favouriteController.create));
-router.get('/:id', asyncHandler(favouriteController.show));
+// router.get('/:id', asyncHandler(favouriteController.show));
 router.delete('/:id', asyncHandler(favouriteController.remove));
 
 export default router as Router;
