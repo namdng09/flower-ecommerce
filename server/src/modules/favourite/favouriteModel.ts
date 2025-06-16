d90c4ab9ebd2d6f91ae16810fef05a62ebe36d33
lnvh <lenguyenviethung1509@gmail.com>
2025-06-16 21:20:56 +0700

    configure favouriteModel

--- server/src/modules/favourite/favouriteModel.ts
+++ server/src/modules/favourite/favouriteModel.ts
@@ -1,20 +1,23 @@
-import mongoose, { Schema, Document , Model    } from "mongoose";
+import mongoose, { Schema, Document, Model } from 'mongoose';
 
 export interface IFavourite {
   _id: string;
   createdAt: Date;
   updatedAt: Date;
   userId: string;
-  products: string[]; // Replace with a specific Product type if available
+  products: mongoose.Types.ObjectId[];
 }
 
 const FavouriteSchema = new Schema<IFavourite>(
   {
     userId: { type: String, required: true, trim: true },
-    products: [{ type: String, required: true }],
+    products: [{ type: Schema.Types.Array, ref: 'Product', required: true }]
   },
   { timestamps: true }
 );
 
-const FavouriteModel: Model<IFavourite> = mongoose.model<IFavourite>("Favourite", FavouriteSchema);
+const FavouriteModel: Model<IFavourite> = mongoose.model<IFavourite>(
+  'Favourite',
+  FavouriteSchema
+);
 export default FavouriteModel;
