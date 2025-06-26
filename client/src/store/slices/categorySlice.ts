341f988b3866bf571e5569a6a4893e042b938bd5
lnvh <lenguyenviethung1509@gmail.com>
2025-06-26 14:12:28 +0700

    feat: add categorySlice

---
+++ client/src/store/slices/categorySlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchCategories = createAsyncThunk(
+  'categories/fetchCategories',
+  async () => {
+    const res = await fetch('http://localhost:8000/api/categories');
+    const data = await res.json();
+    return data.data;
+  }
+);
+
+const categorySlice = createSlice({
+  name: 'categories',
+  initialState: {
+    items: [],
+    loading: false,
+    error: null
+  },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchCategories.pending, state => {
+        state.loading = true;
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  }
+});
+
+export default categorySlice.reducer;
