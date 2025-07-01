ec028eca8e85290abbebb0e48faecbb7c4d89830
namdng09 <namduong090104@gmail.com>
2025-07-01 17:03:13 +0700

    fix: does not have user id in access token

--- server/src/utils/passport.ts
+++ server/src/utils/passport.ts
@@ -1,6 +1,7 @@
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import UserModel from '~/modules/user/userModel';
+//
 
 const configurePassport = () => {
   const options = {
