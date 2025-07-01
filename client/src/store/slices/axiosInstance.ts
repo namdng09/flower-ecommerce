ab3ebe1ccf7ec01da58bc6691e9b490db8bff7e7
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-01 16:54:24 +0700

    chore: code

--- client/src/store/slices/axiosInstance.ts
+++ client/src/store/slices/axiosInstance.ts
@@ -1,26 +1,3 @@
-// // ~/config/axiosInstance.ts
-// import axios from 'axios';
-
-// const axiosInstance = axios.create({
-//   baseURL: 'http://localhost:8000/api',
-//   withCredentials: true
-// });
-
-// axiosInstance.interceptors.request.use(config => {
-//   const token =
-//     localStorage.getItem('accessToken') ||
-//     sessionStorage.getItem('accessToken');
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return config;
-// });
-
-// export default axiosInstance;
-
-// axiosConfig.ts
 import axios from 'axios';
 
 const axiosInstance = axios.create({
