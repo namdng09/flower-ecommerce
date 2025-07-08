6c717444592a77350a153c1ab52b502a6d1d93f5
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-09 00:34:44 +0700

    chore: user profile

--- client/src/components/profile/ProfileC.tsx
+++ client/src/components/profile/ProfileC.tsx
@@ -107,7 +107,7 @@ const EditProfile: React.FC = () => {
   };
 
   return (
-    <div className="max-w-6xl mx-auto px-4 py-10 text-black space-y-10 mt-50">
+    <div className="max-w-6xl mx-auto px-4 py-10 text-black space-y-10 mt-40">
       {/* USER INFO */}
       <div className="bg-white border border-gray-200 rounded-lg shadow-md p-6">
         <h2 className="text-xl font-bold text-center mb-6 flex items-center justify-center gap-2">
