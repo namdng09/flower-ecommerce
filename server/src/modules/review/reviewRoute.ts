8f20da6a05e826ec071156dc7abc54e156457cbc
ducndahe181048 <144119087+ducndahe181048@users.noreply.github.com>
2025-07-11 14:49:12 +0700

    Update reviewRoute.ts

--- server/src/modules/review/reviewRoute.ts
+++ server/src/modules/review/reviewRoute.ts
@@ -7,8 +7,8 @@ const router: Router = express.Router();
 router.get('/', asyncHandler(reviewController.list));
 router.post('/', asyncHandler(reviewController.create));
 
-router.get('/:reviewId', asyncHandler(reviewController.show));
 router.get('/product/:productId', asyncHandler(reviewController.getByProduct));
+router.get('/:reviewId', asyncHandler(reviewController.show));
 router.patch('/:reviewId', asyncHandler(reviewController.update));
 router.delete('/:reviewId', asyncHandler(reviewController.delete));
 
