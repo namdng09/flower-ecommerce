b9456741d0be908d9f7baf96925c2d68f8fcb32e
lnvh <lenguyenviethung1509@gmail.com>
2025-07-12 23:16:17 +0700

    fix: hide sidebar when shop login

--- client/src/layouts/shopLayout.tsx
+++ client/src/layouts/shopLayout.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import Sidebar from '~/components/shop/sidebar/Sidebar';
 
 const ShopLayout = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/shop/login';
+
   return (
     <div className='min-h-screen flex'>
-      <Sidebar />
+      {!isLoginPage && <Sidebar />}
       <main className='flex-1 p-6'>
         <Outlet />
       </main>
